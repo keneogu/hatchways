26b59e6a120555390341c78d88228e86fc3c8206
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-07-28 22:39:03 +0100

    Add reducers to fetch and update store

--- src/redux/studentSlice.js
+++ src/redux/studentSlice.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const fetchAsyncStudent = createAsyncThunk("student/fetchAsyncStudent", async () => {
@@ -11,5 +11,34 @@ export const fetchAsyncStudent = createAsyncThunk("student/fetchAsyncStudent", a
 	}
 });
 
-export default studentSlice.reducer;
+const initialState = {
+	student: {},
+	isLoading: false,
+	isError: false,
+}
 
+const studentSlice = createSlice({
+	name: 'student',
+	initialState,
+	extraReducers: {
+		[fetchAsyncStudent.pending]: (state) => {
+			state.isLoading = true;
+			console.log("Pending");
+		},
+		[fetchAsyncStudent.fulfilled]: (state, { payload }) => {
+			console.log(state);
+			console.log("fetched successfully");
+			state.student = payload;
+		},
+		[fetchAsyncStudent.rejected]: (state) => {
+			state.isError = true;
+			console.log("Rejected");
+		},
+	},
+});
+
+export const getAllStudents = (state) => state.student.student;
+export const getLoading = (state) => state.student.isLoading;
+export const getError = (state) => state.student.isError;
+
+export default studentSlice.reducer;

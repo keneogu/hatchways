3e906f427d7b16113094207f46ac7c47f1ab3e32
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-07-28 22:39:22 +0100

    configure store

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import studentReducer from'./redux/studentSlice';
+
+export const store = configureStore({
+  reducer: {
+    student: studentReducer,
+  },
+});

c9e3d35a0cc082be51b5071e6f2dfb86f7293b78
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-07-28 22:54:16 +0100

    Import Home component on App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './Container/Home';
+
 
 function App() {
   return (
     <div className="App">
-      This is a new project
+      <Home />
     </div>
   );
 }

4a7a0cdcfae1d9ffcfbb9c701300702a0a7763c0
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-07-31 17:22:27 +0100

    Add className to studentCard items

--- src/Container/StudentCard.js
+++ src/Container/StudentCard.js
@@ -5,17 +5,21 @@ const StudentCard = ({firstName,lastName,email,company,skill,pic,grades}) => {
 	let average = grades.map(Number).reduce((x,y) => x+y) / 2;
 
 	return (
-		<div>
-			<div>
-				<div>
-					<img src={pic} alt="profile" />
+		<div className="card-container">
+			<div className="inner-card">
+				<div className="img-contain">
+					<img src={pic} alt="profile" className="img"/>
 				</div>
-				<h2>{firstName}</h2>
-				<h2>{lastName}</h2>
-				<p>{email}</p>
-				<p>{company}</p>
-				<p>{skill}</p>
-				<p>{average}%</p>
+				<div className="text-contain">
+					<div className="h-contain">
+						<h2 className="h-2">{firstName}</h2>
+						<h2 className="h-2-1">{lastName}</h2>
+					</div>
+					<p className="p-tag">Email: {email}</p>
+					<p className="p-tag">Company: {company}</p>
+					<p className="p-tag">Skill: {skill}</p>
+					<p className="p-tag">Average: {average}%</p>
+				</div>
 			</div>
 		</div>
 	)

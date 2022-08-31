1c7e78248c8c9befd54dd4b6072547c4b8027b7e
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-08-31 05:48:28 +0100

    Change Icon class name to className to avoid react errors

--- src/Icons.js
+++ src/Icons.js
@@ -1,6 +1,6 @@
 export const AddIcon = () => {
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+		<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
 </svg>
 	)
@@ -8,7 +8,7 @@ export const AddIcon = () => {
 
 export const MinusIcon = () => {
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+		<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
   <path stroke-linecap="round" stroke-linejoin="round" d="M20 12H4" />
 </svg>
 	)
